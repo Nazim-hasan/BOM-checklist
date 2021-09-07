abfe381e3e9abc0ee2adcde607352542cc393f37
Nazim Hasan <nazimhasan124073@gmail.com>
2021-09-08 00:21:04 +0600

    Problem 2 & 3 added

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // ১. setTimeout এর ভিতরে একটা কিছু কনসোল লগ করবে যেটা সাড়ে তিন সেকেন্ড পরে আউটপুট দেখাবে। ,
 setTimeout(showContent=>console.log('I am problem number one'),5000);
 // ২. prompt দিয়ে ইউজারের কাছ থেকে একটা সংখ্যা নাও। সেটার সাথে ২০০যোগ করো। যোগ করতে গেলে parse করা লাগলে সেটা করো। তারপর সেই রেজাল্টকে alert দিয়ে আউটপুট দেখাও 
-
+const AskInput=()=>{
+    let numberInput=prompt("Enter an interger number");
+    numberInput=parseInt(numberInput);
+    if(numberInput){
+        let result=numberInput+200;
+        console.log(result);
+    }
+}
 // ৩. ব্রাউজারে একটা confirm করে জিজ্ঞেস করো তুমি কি তোমার ওয়েবসাইট এর লোকেশন দেখতে চাও। যদি বলে দেখতে চাই তাহলে সেই ওয়েবসাইট এর লোকেশন এর href জিনিসটা কনসোল লগ করে দেখাও 
-
+const showURL=()=>{
+    const isShow=confirm("Want to see URL ?");
+    if(isShow){
+        console.log(window.location.href);
+    }
+}
 // ৪. Cookies কি জিনিস। এইটা দিয়ে কি করা হয়। সেটা ইংরেজিতে ৫ থেকে ৭ লাইনের মধ্যে লিখে ফেলো। 
 
 // ৫. local storage আর session storage এর মধ্যে তিনটা পার্থক্য লিখে ফেলো। ইংরেজিতে। (নিজের মতো করে লিখবে )
